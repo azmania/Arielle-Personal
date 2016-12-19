286d91d45fa6855d2e1c80fe65ceb17c434024f0
1ainabeari <1ainabeari@gws03904.ihdsb.CA>
2016-12-19 14:44:26 -0500

    Made Superhero stuff.

---
+++ src/Random/SuperheroQuiz.java
@@ -0,0 +1,35 @@
+/*
+ * Arielle
+ * Mon. Dec. 19, 2016
+ * Version 1.0
+ * Which hero are you?. 
+ */
+package Random;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 1ainabeari
+ */
+public class SuperheroQuiz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Objects
+        Scanner input = new Scanner(System.in);
+        
+        //Variables 
+        //characters
+        int storm, msmarvel, wonderwoman;
+        
+        //choosing
+        int choice; 
+        
+        //Input
+        
+    }
+    
+}
