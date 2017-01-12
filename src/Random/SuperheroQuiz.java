cabe94924f1f27c82f0a805d73a1a34ac356c46c
1ainabeari <1ainabeari@gws03904.ihdsb.CA>
2017-01-12 13:24:33 -0500

    Gender.

--- src/Random/SuperheroQuiz.java
+++ src/Random/SuperheroQuiz.java
@@ -23,13 +23,40 @@ public class SuperheroQuiz {
         
         //Variables 
         //characters
-        int storm, msmarvel, wonderwoman;
+        int storm = 0;
+        int msmarvel = 0;
+        int wonderwoman = 0;
         
         //choosing
-        int choice; 
+        int choice = 0; 
         
         //Input
+        sopl("Which Super Hero Are You??");
+        sopl("\n\n\n Ready? (Press Enter)");
+        input.nextLine();
+        sopl("1. Female(1) or Male(2)? Or Both(3)?");
+        choice += input.nextInt();
+        switch(choice){
+            case 1:
+                storm += 1;
+                msmarvel += 1;
+                wonderwoman += 1;
+                break;
+            case 2:
+                break;
+            case 3:
+                storm += 1;
+                msmarvel += 1;
+                wonderwoman += 1;
+                break;
+            default:
+                
+        }
         
+    }
+    
+    public static void sopl(String n){
+        System.out.println(n);
     }
     
 }
