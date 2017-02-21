7c304aa174aefb61f566e28d8e1bdca0f1518468
A & J <A & J@DESKTOP-LE50ALB>
2017-02-20 22:54:16 -0500

    Tournament

---
+++ src/CCCPractice/Tournament.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CCCPractice;
+
+/**
+ *
+ * @author A & J
+ */
+public class Tournament {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int w = 0;
+        while (document.hasNext) {
+            String n = document;
+            switch (n) {
+                case "W":
+                    w += 1;
+                    break;
+            }
+
+            doc.nextLine();
+        }
+
+        if (w >= 5) {
+            System.out.println("1");
+        } else if (w >= 3) {
+            System.out.println("2");
+        } else if (w >= 1) {
+            System.out.println("3");
+        } else {
+            System.out.println(-1);
+        }
+    }
+
+}
